254ed4783e741f7f7082ad8cdbca9c71d6963c4b
vishu yelisetty <vishu yelisetty>
2016-01-13 16:37:15 -0800

    initial project

---
+++ desktop/src/com/carnagestudios/projectjawn/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.carnagestudios.projectjawn.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.carnagestudios.projectjawn.Driver;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Driver(), config);
+	}
+}
